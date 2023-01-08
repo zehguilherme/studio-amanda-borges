8fb360179e0a61e172feb9fa845f07e16c8496df
José Guilherme <jgtomaine@hotmail.com>
2023-01-08 17:37:04 -0300

    Add header (mobile)

---
+++ web/src/components/Icon/index.jsx
@@ -0,0 +1,21 @@
+import { ReactComponent as arrowUp } from "./svgs/arrow-up.svg";
+import { ReactComponent as back } from "./svgs/back.svg";
+import { ReactComponent as email } from "./svgs/email.svg";
+import { ReactComponent as hamburger } from "./svgs/hamburguer.svg";
+import { ReactComponent as instagram } from "./svgs/instagram.svg";
+import { ReactComponent as whatsapp } from "./svgs/whatsapp.svg";
+
+const icons = {
+  arrowUp: arrowUp,
+  back: back,
+  instagram: instagram,
+  whatsapp: whatsapp,
+  email: email,
+  hamburger: hamburger,
+};
+
+export function Icon({ name, ...props }) {
+  const Element = icons[name];
+
+  return <Element {...props} />;
+}
