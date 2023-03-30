e69c898ecfbb77060ea9f2fcdc7c8afe1c2e3e06
José Guilherme <jgtomaine@hotmail.com>
2023-03-30 18:28:23 -0300

    Migrate project page to nextjs

---
+++ web-nextjs/src/components/LightboxNextJsImage/index.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import {
+  isImageFitCover,
+  isImageSlide,
+  useLightboxProps,
+} from "yet-another-react-lightbox/core";
+
+export function LightboxNextJsImage({ slide, rect }) {
+  const { imageFit } = useLightboxProps().carousel;
+  const cover = isImageSlide(slide) && isImageFitCover(slide, imageFit);
+
+  const width = !cover
+    ? Math.round(
+        Math.min(rect.width, (rect.height / slide.height) * slide.width)
+      )
+    : rect.width;
+
+  const height = !cover
+    ? Math.round(
+        Math.min(rect.height, (rect.width / slide.width) * slide.height)
+      )
+    : rect.height;
+
+  return (
+    <div style={{ position: "relative", width, height }}>
+      <Image
+        fill
+        alt=""
+        src={slide}
+        loading="eager"
+        placeholder="blur"
+        blurDataURL="https://www.datocms-assets.com/85603/1674918096-2-2.jpg"
+        draggable={false}
+        style={{ objectFit: cover ? "cover" : "contain" }}
+        sizes={`${Math.ceil((width / window.innerWidth) * 100)}vw`}
+      />
+    </div>
+  );
+}
