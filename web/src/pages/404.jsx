1270acdb595a85a91612d0cb3666f3b309818c87
José Guilherme <jgtomaine@hotmail.com>
2023-05-16 19:07:50 -0300

    Change open graph information

--- web/src/pages/404.jsx
+++ web/src/pages/404.jsx
@@ -7,8 +7,53 @@ export default function NotFound() {
   return (
     <>
       <Head>
-        <title>Studio Amanda Borges | Página não encontrada</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Title */}
+        <title>Studio Amanda Borges | Página não encontrada</title>
+        <meta
+          name="title"
+          content="Studio Amanda Borges | Página não encontrada"
+        />
+        <meta
+          property="twitter:title"
+          content="Studio Amanda Borges | Página não encontrada"
+        />
+
+        {/* Description */}
+        <meta
+          name="description"
+          content="Não foi encontrado nenhum conteúdo para essa página!"
+        />
+        <meta
+          property="og:description"
+          content="Não foi encontrado nenhum conteúdo para essa página!"
+        />
+        <meta
+          property="twitter:description"
+          content="Não foi encontrado nenhum conteúdo para essa página!"
+        />
+
+        {/* URL */}
+        <meta
+          property="og:url"
+          content="https://studioamandaborges.vercel.app"
+        />
+        <meta
+          property="twitter:url"
+          content="https://studioamandaborges.vercel.app"
+        />
+
+        {/* Image */}
+        <meta property="twitter:image" content="/not-found-page" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="og:image" itemProp="image" content="/not-found-page" />
+        <meta property="og:image:width" content="526" />
+        <meta property="og:image:height" content="275" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:type" content="website" />
       </Head>
 
       <main
