9d0b6d5c19bb614cefd1b72c26a4f77b760b0a5e
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2023-07-19 19:12:13 -0300

    Add unit test to footer component

---
+++ web/src/components/Footer/Footer.test.js
@@ -0,0 +1,66 @@
+import { render, screen } from "@testing-library/react";
+
+import { Footer } from ".";
+import { Email } from "../icons/Email";
+import { Instagram } from "../icons/Instagram";
+import { Logo } from "../icons/Logo";
+import { WhatsApp } from "../icons/WhatsApp";
+
+describe("About component", () => {
+  it("should render a link to the Home page", () => {
+    render(<Footer />);
+
+    const homeLink = screen.getByLabelText("Navegar para a página Home");
+    expect(homeLink).toHaveAttribute("href", "/");
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  it("should render a logo svg", () => {
+    const { container } = render(<Logo />);
+
+    const logoElement = container.querySelector("svg");
+    expect(logoElement).toHaveAttribute("name", "logo");
+    expect(logoElement).toBeVisible();
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  it("should render 3 social media nav links with their logos (Instagram, WhatsApp and E-mail)", () => {
+    render(<Footer />);
+
+    // Instagram
+    const instagramLink = screen.getByLabelText("Navegar para o Instagram");
+    expect(instagramLink).toHaveAttribute(
+      "href",
+      "https://www.instagram.com/studio.amandaborges"
+    );
+
+    const { container: instagramHtmlElement } = render(<Instagram />);
+    const instagramLogo = instagramHtmlElement.querySelector("svg");
+    expect(instagramLogo).toHaveAttribute("name", "instagram");
+    expect(instagramLogo).toBeVisible();
+    expect(instagramLogo).toBeInTheDocument();
+
+    // WhatsApp
+    const whatsappLink = screen.getByLabelText("Navegar para o WhatsApp");
+    expect(whatsappLink).toHaveAttribute("href", "https://wa.me/5514998695347");
+
+    const { container: whatsappHtmlElement } = render(<WhatsApp />);
+    const whatsappLogo = whatsappHtmlElement.querySelector("svg");
+    expect(whatsappLogo).toHaveAttribute("name", "whatsapp");
+    expect(whatsappLogo).toBeVisible();
+    expect(whatsappLogo).toBeInTheDocument();
+
+    // E-mail
+    const emailLink = screen.getByLabelText("Enviar um e-mail");
+    expect(emailLink).toHaveAttribute(
+      "href",
+      "mailto:amanda_leticiah@hotmail.com"
+    );
+
+    const { container: emailHtmlElement } = render(<Email />);
+    const emailLogo = emailHtmlElement.querySelector("svg");
+    expect(emailLogo).toHaveAttribute("name", "email");
+    expect(emailLogo).toBeVisible();
+    expect(emailLogo).toBeInTheDocument();
+  });
+});
--- web/src/pages/index.jsx
+++ web/src/pages/index.jsx
@@ -13,11 +13,8 @@ import { About } from "@/components/About";
 import { Header } from "@/components/Header";
 import { ProjectCard } from "@/components/ProjectCard";
 import { ScrollUpButton } from "@/components/ScrollUpButton";
-import { Email } from "@/components/icons/Email";
-import { Instagram } from "@/components/icons/Instagram";
-import { Logo } from "@/components/icons/Logo";
-import { WhatsApp } from "@/components/icons/WhatsApp";
 
+import { Footer } from "@/components/Footer";
 import "swiper/css/bundle";
 
 export async function getServerSideProps(context) {
@@ -313,46 +310,7 @@ export default function Home({ carouselData, projectsData, aboutData }) {
         </section>
       </main>
 
-      <footer className="bg-pink" id="contato">
-        <div className="container mx-auto flex items-center justify-between pr-3">
-          <Link href="/" aria-label="Navegar para a página Home">
-            <Logo className="w-[150px]" />
-          </Link>
-
-          <ul className="flex items-center space-x-1 text-white-white1">
-            <li className="flex items-center">
-              <Link
-                href="https://www.instagram.com/studio.amandaborges/"
-                target="_blank"
-                className="inline-block p-[10px]"
-                aria-label="Navegar para o Instagram"
-              >
-                <Instagram className="w-7" />
-              </Link>
-            </li>
-            <li className="flex items-center">
-              <Link
-                href="https://wa.me/5514998695347"
-                target="_blank"
-                className="inline-block p-[10px]"
-                aria-label="Navegar para o WhatsApp"
-              >
-                <WhatsApp className="w-7" />
-              </Link>
-            </li>
-            <li className="flex items-center">
-              <Link
-                href="mailto:amanda_leticiah@hotmail.com"
-                target="_blank"
-                className="inline-block p-[10px]"
-                aria-label="Enviar um e-mail"
-              >
-                <Email className="w-7" />
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </footer>
+      <Footer />
 
       <ScrollUpButton />
 
